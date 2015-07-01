872727dc9e734701a883459f80204965f40d2fcc
nmelehan <nmelehan@gmail.com>
2015-06-30 21:00:05 -0400

    Resizing preserves canvas context, with a bug
    The sketch being drawn can ‘stutter’ when resizing quickly. If a line
    is being drawn at a constant velocity, gaps will appear in the line
    according to how quickly the resize occurs.

--- 2015/web-export/load_background_sketch.js
+++ 2015/web-export/load_background_sketch.js
@@ -9,13 +9,32 @@ backgroundSketch.setSketchSize = function() {
 }
 
 $(document).ready(function() {
-	var canvas = document.getElementById("background_sketch");
+	backgroundSketch.canvas = document.getElementById("background_sketch");
 	// attaching the sketchProc function to the canvas
-	backgroundSketch.processingInstance = new Processing(canvas, backgroundSketchCode);
+	backgroundSketch.processingInstance = new Processing(backgroundSketch.canvas, backgroundSketchCode);
 	$("#background_sketch").appendTo("#homeBlockField");
 	backgroundSketch.setSketchSize();
 });
 
-$(window).resize(function() {
+backgroundSketch.resize = function() {
+	// save image data so it can be applied to context after resize
+	var sketchCanvasContext = backgroundSketch.canvas.getContext("2d");
+	var oldWidth = backgroundSketch.canvas.width;
+	var oldHeight = backgroundSketch.canvas.height;
+	var imageData = sketchCanvasContext.getImageData(0, 0, oldWidth, oldHeight);
+
+	// resize
 	backgroundSketch.setSketchSize();
+
+	// apply saved image data
+	var newWidth = backgroundSketch.canvas.width;
+	var newHeight = backgroundSketch.canvas.height;
+
+	var imageDataX = (newWidth - oldWidth)/2;
+	var imageDataY = (newHeight - oldHeight)/2;
+	sketchCanvasContext.putImageData(imageData, imageDataX, imageDataY);
+}
+
+$(window).resize(function() {
+	backgroundSketch.resize();
 });
