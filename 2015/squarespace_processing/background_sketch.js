7d48cf3d052583f08b8fedcc169fb9b9dd3a8709
nmelehan <nmelehan@gmail.com>
2015-07-02 01:08:39 -0400

    Canvas element is added in javascript
    And background_sketch.js and load_background_sketch.js are merged

--- 2015/squarespace_processing/background_sketch.js
+++ 2015/squarespace_processing/background_sketch.js
@@ -1,3 +1,53 @@
+//
+// Utility code for loading and managing sketch
+//
+
+var backgroundSketch = {};
+
+backgroundSketch.setSketchSize = function() {
+    var parentWidth = $("#slideshow").innerWidth();
+    var parentHeight = $("#slideshow").innerHeight();
+    backgroundSketch.processingInstance.size(parentWidth, parentHeight);
+    $("#background_sketch").width(parentWidth);
+    $("#background_sketch").height(parentHeight);
+}
+
+$(document).ready(function() {
+    $("#slideshow").append("<canvas id='background_sketch'></canvas>");
+    backgroundSketch.canvas = document.getElementById("background_sketch");
+    // attaching the sketchProc function to the canvas
+    backgroundSketch.processingInstance = new Processing("background_sketch", backgroundSketchCode);
+    backgroundSketch.setSketchSize();
+});
+
+backgroundSketch.resize = function() {
+    // save image data so it can be applied to context after resize
+    var sketchCanvasContext = backgroundSketch.canvas.getContext("2d");
+    var oldWidth = backgroundSketch.canvas.width;
+    var oldHeight = backgroundSketch.canvas.height;
+    var imageData = sketchCanvasContext.getImageData(0, 0, oldWidth, oldHeight);
+
+    // resize
+    backgroundSketch.setSketchSize();
+
+    // apply saved image data
+    var newWidth = backgroundSketch.canvas.width;
+    var newHeight = backgroundSketch.canvas.height;
+
+    var imageDataX = (newWidth - oldWidth)/2;
+    var imageDataY = (newHeight - oldHeight)/2;
+    sketchCanvasContext.putImageData(imageData, imageDataX, imageDataY);
+}
+
+$(window).resize(function() {
+    backgroundSketch.resize();
+});
+
+
+//
+// Processing code for sketch
+//
+
 function backgroundSketchCode(processing) {
     var Point = (function() {
         function Point() {
